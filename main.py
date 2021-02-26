6333903ef97b30720c5ff2356fd92db6a97d2e4b
Milana <milanasirozhova@yandex.ru>
2021-02-27 00:14:12 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+import random
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtWidgets import QWidget, QApplication, QSlider
+from UI import Ui_MainWindow
+
+
+class MainWindow(Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.do_paint = False
+        self.diam = 10
+        self.pb.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_face(qp)
+            qp.end()
+
+    def draw_face(self, qp):
+        pen = QPen(Qt.yellow)
+        qp.setPen(pen)
+        for i in range(3):
+            diam = random.randint(20, 50)
+            qp.drawEllipse(random.randint(20, 620), random.randint(20, 520), diam, diam)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wnd = MainWindow()
+    wnd.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
